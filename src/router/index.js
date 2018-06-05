4d37fa2b5c8c2afefe8aaf60f9881dd9c1d2f0af
Kesara Wimal <kesarawimal9@gmail.com>
2018-06-06 02:03:42 +0530

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import TodoList from '@/components/TodoList'
-import Navbar from '@/components/Navbar'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '',
       component: TodoList
-    },
-    {
-      path: '/nav',
-      component: Navbar
     }
   ]
 })
