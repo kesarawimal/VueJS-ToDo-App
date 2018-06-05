07c14e8be04ef3f9073b770a46f0e0117642100e
Kesara Wimal <kesarawimal9@gmail.com>
2018-06-06 03:36:31 +0530

    Adding files.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,29 +1,34 @@
 <template>
    <div>
     <!-- // JavaScript expressions in Vue are enclosed in double curly brackets. -->
-    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
-    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <div class="ui label">
+      <i class="paper plane icon"></i>
+    <span>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</span>
+    </div>
+    <div class="ui label">
+      <i class="calendar alternate icon"></i>
+      <span>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</span>
+</div>
     <todo v-on:delete-todo="deleteTodo" v-for="todo in todos" :todo.sync="todo" :key="todo"></todo>
   </div>
 </template>
 
 <script>
-import Todo from './Todo'
+import Todo from "./Todo";
 
 export default {
-    props: ['todos'],
-    components: {
-      Todo,
+  props: ["todos"],
+  components: {
+    Todo
   },
   methods: {
     deleteTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos.splice(todoIndex, 1);
-    },
-  },
-}
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
